8ebd7af9502b40bc3997a387e277d3f846519b01
clyg <45275327+clyg@users.noreply.github.com>
2022-11-11 22:32:59 +0800

    新增加对于搜索文件的多协程

--- walkDir.go
+++ walkDir.go
@@ -42,6 +42,40 @@ func walkDir(dir string, n *sync.WaitGroup, dirchan chan<- string, tokenChan cha
 	}
 }
 
+func FindAllFile(dir string) []string {
+	fileSlice := make([]string, 0)
+	fileChan := make(chan string, 32)
+	tokenChan := make(chan struct{}, 32)
+	var n sync.WaitGroup
+	n.Add(1)
+	go func() {
+		walkFile(dir, &n, fileChan, tokenChan)
+	}()
+	go func() {
+		n.Wait()
+		close(fileChan)
+	}()
+	for fileName := range fileChan {
+		fileSlice = append(fileSlice, fileName)
+	}
+	return fileSlice
+}
+
+func walkFile(dir string, n *sync.WaitGroup, fileChan chan<- string, tokenChan chan struct{}) {
+	defer n.Done()
+	for _, entry := range dirents(dir) {
+		subDir := filepath.Join(dir, entry.Name())
+		if entry.IsDir() {
+			n.Add(1)
+			go walkFile(subDir, n, fileChan, tokenChan)
+		} else {
+			tokenChan <- struct{}{}
+			fileChan <- subDir
+			<-tokenChan
+		}
+	}
+}
+
 func dirents(dir string) []fs.DirEntry {
 	entries, err := os.ReadDir(dir)
 	if err != nil {
